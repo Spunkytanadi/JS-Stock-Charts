6291fd265d8388823fc1662d4c94b6985a6af593
Marceilla Norman <101609514+Spunkytanadi@users.noreply.github.com>
2022-06-29 15:42:30 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -6,4 +6,38 @@ async function main() {
 
 }
 
+const { GME, MSFT, DIS, BNTX } = mockData;
+
+const stocks = [GME, MSFT, DIS, BNTX];
+
+stocks.forEach( stock => stock.values.reverse())
+
+new Chart(timeChartCanvas.getContext('2d') => {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.reverse().map(value => value.datetime),
+        datasets: stocks.map(stock => ({
+            label: stock.meta.symbol,
+            data: stock.values.reverse().map(value => parseFloat(value.high)),
+            backgroundColor: getColor(stock.meta.symbol),
+            borderColor: getColor(stock.meta.symbol),
+        }))
+    }
+});
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+
 main()
